89aebd3ab8ba53cbeba1ae2bf0c9f5cdcc62aeac
Sergio Hernandez <herna518@purdue.edu>
2022-10-02 17:40:48 -0400

    deleted page1 and two and added two buttons

--- Chariot/src/routes/home-page-generic.js
+++ Chariot/src/routes/home-page-generic.js
@@ -1,13 +1,17 @@
 import './home-page-generic.css';
 import SignInButton from '../components/buttons/SignInButton';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function HomePageGeneric() {
 return (
-<div className="container">
-  <h1>CHARIOT</h1>
-  <div className="inner-container">
-   <SignInButton/>
-  </div>
- </div>
+  <Router>
+    <div className="container">
+      <h1>CHARIOT</h1>
+    <div className="inner-container">
+      <SignInButton/>
+    </div>
+    </div>
+  </Router>
 );
 }
 export default HomePageGeneric;
