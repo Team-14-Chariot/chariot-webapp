1f7692ebd5947d30a38e521b4f3be54f0d8f1ff3
Sergio Hernandez <herna518@purdue.edu>
2022-10-10 16:49:35 -0400

    updated event information

--- Chariot/src/routes/create-event-page.js
+++ Chariot/src/routes/create-event-page.js
@@ -3,6 +3,7 @@ import Header from '../components/views/Header';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import GenericSubmitButton from '../components/buttons/GenericSubmitButton'
+import SelectUSState from 'react-select-us-states';
 
 
 function CreateEventPage() {
@@ -11,8 +12,14 @@ function CreateEventPage() {
     const [info, setInfo] = useState({
         name: "",
         address: "",
+        city: "",
+        state: "",
+        zip: "",
         radius: ""
     })
+
+    
+
 
     const [submitted, setSubmitted] = useState(false);
 
@@ -23,6 +30,22 @@ function CreateEventPage() {
     const handleAddressChange = (event) => {
         setInfo({...info, address: event.target.value})
     }
+
+    const handleCityChange = (event) => {
+        setInfo({...info, city: event.target.value})
+    }
+
+    const handleStateChange = (event) => {
+        setInfo({...info, state: event})
+        console.log(event);
+        //console.log(info.state);
+        //console.log(newValue);
+        //this.setNewValue;
+    }
+
+    const handleZipChange = (event) => {
+        setInfo({...info, zip: event.target.value})
+    }
     
     const handleRadiusChange = (event) => {
         setInfo({...info, radius: event.target.value})
@@ -31,6 +54,7 @@ function CreateEventPage() {
     const handleSubmitted = (event) => {
         event.preventDefault();
         setSubmitted(true);
+        //go to the backend
         navigate('../main-page/')
     }
 
@@ -42,8 +66,14 @@ function CreateEventPage() {
         <form className='eventForm'>
             <label>EVENT NAME: </label>
             <input onChange={handleNameChange} type="text" name="name" value={info.name}></input><br></br>
-            <label>EVENT ADDRESS: </label>
+            <label>ADDRESS LINE 1: </label>
             <input onChange={handleAddressChange} type="text" name="address" value={info.address}></input><br></br>
+            <label>CITY: </label>
+            <input onChange={handleCityChange} type="text" name="city" value={info.city}></input><br></br>
+            <label>STATE: </label>
+            <SelectUSState onChange={handleStateChange} /><br></br>
+            <label>ZIP CODE: </label>
+            <input onChange={handleZipChange} type="text" name="zip" value={info.zip}></input><br></br>
             <label>MAX PICKUP RADIUS: </label>
             <input onChange={handleRadiusChange} type="text" name="radius" value={info.radius}></input>  MILES
             <br></br><br></br>
