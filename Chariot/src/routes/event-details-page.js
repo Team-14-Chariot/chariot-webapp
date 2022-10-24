9db1e8a30db5f4b4c90cb4489345d406674c22ba
matthew-s-white <white753@purdue.edu>
2022-10-23 22:21:47 -0400

    finished design and edit feature of event details

--- Chariot/src/routes/event-details-page.js
+++ Chariot/src/routes/event-details-page.js
@@ -60,7 +60,7 @@ function EventDetailsPage() {
     }
 
     const handleStateChange = (event) => {
-        setEditableInfo({...editableInfo, state: event})
+        setEditableInfo({...editableInfo, state: event.target.value})
     }
 
     const handleZipChange = (event) => {
@@ -92,6 +92,30 @@ function EventDetailsPage() {
         <div className='eventDetailsBubble'>
         <div className='eventDetailsTitle'>
             {canEdit ? <input onChange={handleNameChange} defaultValue={editableInfo.name}></input> : <text className='eventDetailsTitleText'>{editableInfo.name}</text>}
+        </div>
+        <div>
+            <br></br>
+            <text className='eventDetailsEventCode'><b>EVENT CODE:</b> {info.eventCode}</text>
+            <text className='eventDetailsRiderLink'><b>RIDER LINK:</b> {info.riderLink}</text>
+        </div>
+        <div className='eventDetailsContent'>
+            <div className='eventDetailsContentText'>
+                <br></br>
+                <text className='eventDetailsAddress'><b>ADDRESS:</b></text>
+                <br></br>
+
+                {canEdit ? <text>&nbsp;&nbsp;&nbsp;&nbsp;<input onChange={handleAddressChange} defaultValue={editableInfo.address}></input>,</text> : <text className='eventDetailsAddressInfo'>&nbsp;&nbsp;&nbsp;&nbsp;{editableInfo.address},</text>}
+                {canEdit ? <text><br></br>&nbsp;&nbsp;&nbsp;&nbsp;<input onChange={handleCityChange} defaultValue={editableInfo.city}></input>,</text> : <text className='eventDetailsAddressInfo'><br></br>&nbsp;&nbsp;&nbsp;&nbsp;{editableInfo.city},</text>}
+                {canEdit ? <text><br></br>&nbsp;&nbsp;&nbsp;&nbsp;<input onChange={handleStateChange} defaultValue={editableInfo.state}></input></text> : <text className='eventDetailsAddressInfo'><br></br>&nbsp;&nbsp;&nbsp;&nbsp;{editableInfo.state}</text>}
+                {canEdit ? <text><br></br>&nbsp;&nbsp;&nbsp;&nbsp;<input onChange={handleZipChange} defaultValue={editableInfo.zip}></input></text> : <text className='eventDetailsAddressInfo'><br></br>&nbsp;&nbsp;&nbsp;&nbsp;{editableInfo.zip}</text>}
+
+                <br></br><br></br>
+                <text className='eventDetailsAddress'><b>MAX RIDE RADIUS:</b> </text> {canEdit ? <text> <input onChange={handleRadiusChange} defaultValue={editableInfo.radius}></input> miles</text> : <text className='eventDetailsAddressInfo'> {editableInfo.radius} miles</text>}
+            </div>
+            <div className='eventDetailsContentMap'>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <text>&#91;MAP WILL GO HERE&#93;</text>
+            </div>
         </div>
         <div>
             {canEdit ? <GenericSubmitButton onClickFunction={handleSubmitted}/> : <button className='eventDetailsEditButton' onClick={handleEditPressed}>EDIT</button>}
