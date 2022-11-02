34c243bb8aa1d6d55d45e06c2ae0c06150e78a31
matthew-s-white <white753@purdue.edu>
2022-11-02 16:52:14 -0400

    updated UI productwide

--- Chariot/src/components/views/Event.js
+++ Chariot/src/components/views/Event.js
@@ -13,14 +13,29 @@ function Event(eventName, eventAddr, eventRadius, eventCode, eventAcceptingRides
 
 
     return(
-    <div className="eventContainer" onClick={() => handleClicked(eventCode)}>
-        <div>
-            EVENT NAME: {eventName}         EVENT CODE: {eventCode} <br></br>
-            EVENT ADDRESS: {eventAddr}<br></br>
-            MAX RADIUS: {eventRadius} miles
-            {eventAcceptingRides ? <EndButton onClickFunction={handleEnded}/> : null}
+    <div className="eventContainer">
+        <div className='event_header_container'>
+            <div className='event_name'>
+                {eventName}
+            </div>
+            <div className='event_end_button'>
+                {eventAcceptingRides ? <EndButton onClickFunction={handleEnded}/> : null}
+            </div>
+            <div className='event_code'>
+                {eventCode}
+            </div>
         </div>
-        <br></br>
+        <div className='event_body_container'>
+            <div className='event_address'>
+                ADDRESS:
+            </div>
+            <div className='event_address_info'>
+                {eventAddr}
+            </div>
+            <div className='event_click_to_view' onClick={() => handleClicked(eventCode)}>
+                &#40; CLICK TO VIEW DETAILS &#41;
+            </div>
+        </div>
     </div>
     )
 }
