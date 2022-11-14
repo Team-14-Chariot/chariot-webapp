2b3e7a79cf7893c6c8b9f56097d1f3d505ceabaa
Sergio Hernandez <herna518@purdue.edu>
2022-11-13 21:12:20 -0500

    worked on the UI for displaying rides

--- Chariot/src/components/views/Ride.js
+++ Chariot/src/components/views/Ride.js
@@ -8,12 +8,26 @@ function Ride(riderName, needRide, inRide, eta, groupSize){
 
     return(
     <div className={inRide ? 'rideContainer' : 'noRideContainer'}>
-        <div>
-            RIDER NAME: {riderName} <br></br>
-            ETA: {eta}<br></br>
-            groupSize: {groupSize}
+        <div className='ride_body_container'>
+            <div className='ride_name'>
+                RIDER NAME:
+            </div>
+            <div className='ride_name_info'>
+                {riderName}
+            </div>
+            <div className='ride_eta'>
+                ETA:
+            </div>
+            <div className='ride_eta_info'>
+                {eta}
+            </div>
+            <div className='rider_size'>
+                groupSize:
+            </div>
+            <div className='ride_size_info'> 
+            {groupSize}
+            </div>
         </div>
-        <br></br>
     </div>
     )
 }
