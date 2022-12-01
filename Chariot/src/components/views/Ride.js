a936e6559b796ebdc882809129bdd05739e66275
Sergio Hernandez <herna518@purdue.edu>
2022-12-01 18:16:17 -0500

    worked on the dropdown as well as displaying rides

--- Chariot/src/components/views/Ride.js
+++ Chariot/src/components/views/Ride.js
@@ -10,22 +10,13 @@ function Ride(riderName, needRide, inRide, eta, groupSize){
     <div className={inRide ? 'rideContainer' : 'noRideContainer'}>
         <div className='ride_body_container'>
             <div className='ride_name'>
-                RIDER NAME:
-            </div>
-            <div className='ride_name_info'>
                 {riderName}
             </div>
-            <div className='ride_eta'>
-                ETA:
-            </div>
-            <div className='ride_eta_info'>
-                {eta}
-            </div>
-            <div className='ride_size'>
-                groupSize:
-            </div>
-            <div className='ride_size_info'> 
-            {groupSize}
+            <text className='ride_eta'> <b>ETA:</b>  <text className='ride_eta_info'>{eta} minutes </text>
+            </text>
+            <div>
+            <text className='ride_size'> <b>groupSize:</b>  <text className='ride_size_info'> {groupSize} </text>
+            </text>
             </div>
         </div>
     </div>
--- Chariot/src/routes/delete-account-page.js
+++ Chariot/src/routes/delete-account-page.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './delete-account-page.css';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/views/Header';
@@ -14,7 +14,7 @@ function DeleteAccountPage() {
     })
 
     const [submitted, setSubmitted] = useState(false);
-    const [correctPassword, setCorrectPassword] = useState(true);
+    const [correctPassword, setCorrectPassword] = useState(false);
 
 
     const handlePasswordChange = (event) => {
@@ -23,19 +23,23 @@ function DeleteAccountPage() {
 
     const handleSubmitted = (event) => {
         event.preventDefault();
-        setSubmitted(true);
-        async function attemptAccountDeletion(){
+        async function checkForPassword(){
             const res = await checkEventOrganizerTupleExists(thisUser.getUserEmail(), password.password);
-            console.log(res);
             if (res.status === "success"){
                 setCorrectPassword(true);
-                console.log("Checking was correct");
+                console.log("correct password");
             } else {
                 setCorrectPassword(false);
-                console.log(thisUser.getUserEmail());
-                console.log(password.password);
+                console.log("incorrect password");
             }
-            if(submitted && correctPassword){
+        }
+        checkForPassword();
+        setSubmitted(true);
+    }
+
+    useEffect(() => {
+        async function attemptAccountDeletion(){
+            if(correctPassword && submitted){
                 //have to delete all events
                 //filter the events by owner 
                 const eventsToDelete = await client.records.getList('events', 1, 100, {filter: `owner = "${thisUser.getUserId()}"`});
@@ -59,51 +63,7 @@ function DeleteAccountPage() {
             }
         }
         attemptAccountDeletion();
-    }
-
-    //useEffect(() => {
-        /*async function deleteAccount(){
-            console.log("in function");
-            if(submitted && correctPassword){
-                //have to do it in the backend
-                console.log(thisUser.getUserId());
-                const result = await client.users.delete(thisUser.getUserId());
-                console.log(result);
-                if (result.status === 'success') {
-                    thisUser.setSignedIn(false);
-                    thisUser.setUserEmail(null);
-                    thisUser.setUserId(null);
-                    navigate("/");
-                } else {
-                    console.log("didn't delete");
-                    return;
-                }
-            }
-        }
-        attemptAccountDeletion();
-    }
-
-    //useEffect(() => {
-        /*async function deleteAccount(){
-            console.log("in function");
-            if(submitted && correctPassword){
-                //have to do it in the backend
-                console.log(thisUser.getUserId());
-                const result = await client.users.delete(thisUser.getUserId());
-                console.log(result);
-                if (result.status === 'success') {
-                    thisUser.setSignedIn(false);
-                    thisUser.setUserEmail(null);
-                    thisUser.setUserId(null);
-                    navigate("/");
-                } else {
-                    console.log("didn't delete");
-                    return;
-                }
-            }
-        }*/
-        //deleteAccount();
-    //}, [correctPassword, submitted, navigate]);
+    }, [submitted, correctPassword, navigate]);
 
 
     return (
@@ -114,9 +74,8 @@ function DeleteAccountPage() {
     <form className='delete_account_page_form'>
         <label>Enter Your Password</label><br></br>
         <input onChange={handlePasswordChange} type="text" name="password" value={password.password}></input><br></br>
-        {!correctPassword ? <div className='errorMessage'>Your password doesn't match.</div> : null}
         <GenericSubmitButton onClickFunction={handleSubmitted} />
-        {submitted ? <div>submitted</div> : null}
+        {!correctPassword && submitted ? <div className='errorMessage'>Your password doesn't match.</div> : null}
     </form>
     </div>
     </body>
